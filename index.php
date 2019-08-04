5ce18559f5e93a8d72d41f6b396d0aa4afc7b22e
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-08-04 21:53:21 +0530

    initial commit of index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'partials/header.php'; ?>
+
+<div class="main-front-page">
+	<div class="row">
+		<div class="col-md-9 col-12">
+			<div class="post-list">
+				<div class="card"></div>
+			</div>
+		</div>
+		<div class="col-md-3 col-12"></div>
+	</div>
+</div>
+
+<?php include 'partials/footer.php'; ?>

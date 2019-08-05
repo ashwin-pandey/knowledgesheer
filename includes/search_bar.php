21843ec62d0060024953852230835477331bc7d0
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-08-05 21:54:24 +0530

    edited search bar layout

--- includes/search_bar.php
+++ includes/search_bar.php
@@ -1,6 +1,6 @@
 <div id="search-wrap" class="fixed-top row justify-content-md-center">
 	<div class="search col-md-6 col-sm-8 ml-auto mr-auto col-11 p-3">
-		<form action="search.php" class="" method="post">
+		<form action="search.php" class="" method="POST">
 			<div class="input-group md-3">
 				<input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="basic-addon2">
 				<div class="input-group-append">

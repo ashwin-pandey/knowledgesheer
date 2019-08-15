5c1a1f0c8f04b2b7df1a9ac15db782c5503ef23a
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-08-16 00:14:28 +0530

    added logo in the footer

--- partials/footer.php
+++ partials/footer.php
@@ -10,6 +10,9 @@
 <!-- Footer -->
 <footer>
 	<div class="container">
+		<div class="row justify-content-center pb-4">
+			<img src="assets/images/logo/logo-1.png" height="" width="200" alt="Knowledge Sheer">
+		</div>
 		<div class="row">
 			<div class="col-lg-8 col-md-10 mx-auto">
 				<ul class="list-inline text-center">

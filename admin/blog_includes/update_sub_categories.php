b5ed9f777aa1803cd95a9bbfe0b4d0d3d49888e3
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-08-18 20:51:58 +0530

    added update file for sub categories

---
+++ admin/blog_includes/update_sub_categories.php
@@ -0,0 +1,67 @@
+<div class="card p-3 mb-3">
+	<form method="POST">
+		<div class="form-group">
+			<label>Edit Sub Category</label>
+			<?php
+
+			if(isset($_GET['edit_sub'])){
+				$sub_cat_id = escape($_GET['edit_sub']);
+				$query = "SELECT * FROM sub_categories WHERE sub_cat_id = $sub_cat_id ";
+				$select_category_id = query($query);
+
+				while($row = mysqli_fetch_assoc($select_category_id)) {
+					$sub_cat_id = $row['sub_cat_id'];
+					$sub_cat_title = $row['sub_cat_title'];
+					$parent_cat_id = $row['parent_cat_id'];
+
+					?>
+					<input value="<?php echo $sub_cat_title; ?>" type="text" class="form-control" name="sub_cat_title">
+				</div>
+				<div class="form-group">
+					<label>Parent Category</label>
+					<select name="parent_id" class="form-control">
+
+						<?php  
+
+						$query = "SELECT * FROM categories";
+						$select_parent = query($query);
+						while ($row = mysqli_fetch_assoc($select_parent)) {
+							$parent_title = $row['cat_title'];
+							$parent_id = $row['cat_id'];
+							if ($parent_id == $parent_cat_id) {
+								echo "<option selected value='$parent_id'>$parent_title</option>";
+							} else {
+								echo "<option value='$parent_id'>$parent_title</option>";
+							}
+						}
+
+						?>
+
+					</select>
+				</div>
+			<?php } } ?>
+
+			<?php
+
+			if(isset($_POST['update_sub_category'])) {
+
+				$update_cat_title = escape($_POST['sub_cat_title']);
+				$parent_cat_id = escape($_POST['parent_id']);
+				$update_query = 'UPDATE sub_categories SET sub_cat_title = ?, parent_cat_id = ? WHERE sub_cat_id = ? ';
+
+				$stmt = mysqli_prepare($connection, $update_query);
+				confirmQuery($stmt);
+				mysqli_stmt_bind_param($stmt, 'ssi', $update_cat_title, $parent_cat_id, $sub_cat_id);
+				mysqli_stmt_execute($stmt);
+				redirect("categories.php");
+
+			}
+
+			?>
+
+			<div class="form-group">
+				<input class="btn btn-primary btn-sm" name="update_sub_category" type="submit" value="Update"></input>
+			</div>
+
+		</form>
+	</div>

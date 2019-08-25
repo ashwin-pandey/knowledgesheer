75e09ecc50cad455dcf6231fc65198bddfc54cc5
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-08-25 17:37:59 +0530

    added more meta tags

--- partials/header.php
+++ partials/header.php
@@ -1,23 +1,24 @@
 <?php session_start(); ?>
 <?php
 if ($page == 'blog_post' || $page == 'category') {
-
 } else {
 	include 'includes/db.php';
 	include './admin/functions.php';
 }
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title><?php echo $title; ?> | Knowledge Sheer</title>
-
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="keywords" content="<?php echo $keywords ?>"/>
-	<meta name="description" content="<?php echo $description ?>"/>
+	<meta name="keywords" content="<?php echo $post_tags ?>"/>
+	<meta name="description" content="<?php echo $post_description ?>"/>
+	<meta property="og:title" content="<?php echo $post_title ?>">
+	<meta property="og:url" content="http://knowledgesheer.com/blog_post.php?p_id=<?php echo $the_post_id; ?>" />
+	<meta property="og:description" content="<?php echo $post_description; ?>" />
+	<meta property="og:site_name" content="Knowledge Sheer" />
+	<meta property="og:image" content="http://knowledgesheer.com/assets/images/blog-images/<?php echo $post_image; ?>" />
 	<meta name="language" content="EN">
 	<meta name="robots" content="index,follow" />
 </head>
@@ -35,6 +36,6 @@ if ($page == 'blog_post' || $page == 'category') {
 	<div class="container">
 
 <a class="top-link hide" href="" id="js-top">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
-  <!-- <span class="screen-reader-text">Back to top</span> -->
+  <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg> -->
+  <svg viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
 </a>

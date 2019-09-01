4d7a9afa1f045490619c234d68b653b8b7bc7361
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-09-01 17:32:37 +0530

    improve post query

--- index.php
+++ index.php
@@ -12,6 +12,28 @@ include 'partials/header.php';
 		<!-- Main Content -->
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2 col-xs-12 mx-auto col-12">
+				<h4>Quotes <small class="float-right"><a href="quotes.php">(View All)</a></small></h4>
+				<hr>
+				<div class="owl-carousel owl-theme">
+					<?php  
+					$query = "SELECT quote_id, quote_author, quote_image, quote_hashtags FROM quotes ORDER BY quote_id DESC LIMIT 10 ";
+					$quote_query = query($query);
+					confirmQuery($quote_query);
+					while ($row = mysqli_fetch_assoc($quote_query)) {
+						$quote_id = $row['quote_id'];
+						$quote_author = $row['quote_author'];
+						$quote_image = $row['quote_image'];
+						$quote_hashtags = $row['quote_hashtags'];
+					?>
+					<div class="item"><img class="img-fluid" src="assets/images/quote-images/<?php echo $quote_image; ?>" alt="<?php echo $quote_hashtags; ?>"></div>
+					<?php } ?>
+				</div>
+				<hr>
+				<h4>Blog Posts</h4>
+				<hr>
+
+<!-- 				<div id="load_data"></div>
+				<div id="load_more_data"></div> -->
 
 				<?php
 
@@ -39,26 +61,16 @@ include 'partials/header.php';
 				} else {
 					$count  = ceil($count /$per_page);
 
-					$query = 'SELECT * FROM blog_posts ORDER BY post_id DESC LIMIT ' .$page_1. ',' .$per_page;
-					$blog_posts = query($query);
-					confirmQuery($blog_posts);
+					$query = "SELECT post_id, post_title, post_description, post_date, post_author, post_content, post_image, post_category_id FROM blog_posts ORDER BY post_id DESC LIMIT ?, ? ";
+					$blog_query = mysqli_prepare($connection, $query);
+					mysqli_stmt_bind_param($blog_query, 'ii', $page_1, $per_page);
+					mysqli_stmt_execute($blog_query);
+					confirmQuery($blog_query);
+					mysqli_stmt_store_result($blog_query);
+					mysqli_stmt_bind_result($blog_query, $post_id, $post_title, $post_description, $post_date, $post_author, $post_content, $post_image, $post_category_id);
 
-					while ($row = mysqli_fetch_assoc($blog_posts)) {
-						$post_id = $row['post_id'];
-						$post_title = $row['post_title'];
-						$post_description = stripcslashes($row['post_description']);
-						$post_date = $row['post_date'];
-						$post_author = $row['post_author'];
-						$post_content = $row['post_content'];
-						$post_image = $row['post_image'];
-						$post_category_id = $row['post_category_id'];
-
-						// $query = 'SELECT user_firstname, user_lastname FROM users WHERE username='.$post_author;
-						// $select_author = query($query);
-						// confirmQuery($select_author);
-						// $row = mysqli_fetch_assoc($select_author);
-						// $user_firstname = $row['user_firstname'];
-						// $user_lastname = $row['user_lastname'];
+					while (mysqli_stmt_fetch($blog_query)) {
+						// Author Query
 						$query = "SELECT user_id, user_firstname, user_lastname FROM users WHERE username = ? ";
 						$user_stmt = mysqli_prepare($connection, $query);
 						mysqli_stmt_bind_param($user_stmt, 's', $post_author);
@@ -68,7 +80,6 @@ include 'partials/header.php';
 						mysqli_stmt_bind_result($user_stmt, $user_id, $user_firstname, $user_lastname);
 						mysqli_stmt_fetch($user_stmt);
 						$user_full_name = $user_firstname . " " . $user_lastname;
-
 				?>
 	
 				<div class="post-preview media">
@@ -81,7 +92,6 @@ include 'partials/header.php';
 							<?php echo $post_title; ?>
 							</a>
 						</h2>
-						
 						<h3 class="post-subtitle">
 						<?php echo substr($post_description, 0, 30) ?>
 						</h3>
@@ -104,12 +114,10 @@ include 'partials/header.php';
 					</div>
 				</div>
 				<hr>
-
 				<?php 
 					}
 				} 
 				?>
-
 				<!-- PAGINATION -->
 				<ul class="pagination justify-content-md-center">
 				<?php 
@@ -140,13 +148,10 @@ include 'partials/header.php';
 
 				?>
 				</ul>
-
 			</div>
 			<div class="col-lg-4 col-md-4 col-12">
 				<?php include 'partials/sidebar.php'; ?>
 			</div>
  		</div>
-<!--	</div>
-</div> -->
 
 <?php include 'partials/footer.php'; ?>

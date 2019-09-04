ac377b1ed482efe1393571623433bf6e4767d4d2
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-09-04 20:30:29 +0530

    init all categories page

---
+++ all_categories.php
@@ -0,0 +1,10 @@
+<?php 
+
+$page = 'all_categories'; 
+$header_title = 'Knowledge Sheer';
+include 'partials/header.php'; 
+
+?>
+<!-- Main Content -->
+<div class="row">
+	<div class="col-md-8 col-md-offset-2 col-xs-12 mx-auto col-12">

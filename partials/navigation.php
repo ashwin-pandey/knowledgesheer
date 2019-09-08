309c0e32d0cedf4f41d15177bd2e7a215f3445ac
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-09-08 22:43:13 +0530

    change sub heading

--- partials/navigation.php
+++ partials/navigation.php
@@ -46,7 +46,7 @@
 				<div class="col-lg-8 col-md-10 mx-auto">
 					<div class="site-heading">
 						<h1><?php echo $header_title; ?></h1>
-						<span class="subheading">A Simple Blog For Sharing Knowledge</span>
+						<span class="subheading">A Place For Sharing Knowledge</span>
 					</div>
 				</div>
 			</div>

89913e0ae8d01aa3f68dc6474b5260132659e8a4
ashwin-pandey <ashwin.pandey11197@gmail.com>
2019-10-03 14:31:01 +0530

    reports

---
+++ admin/partials/analytics_report.php
@@ -0,0 +1,6 @@
+<div class="col-md-6">
+	<iframe width="507.0080862533693" height="313.5" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQT4g_NqJN5LmvV7XMotgMSA2YK4-qb5kKl6fAyFCd_bCZ9yMkqAozIXxfc4tFQ58Crn6qYSKPmV1TT/pubchart?oid=78097899&amp;format=interactive"></iframe>
+</div>
+<div class="col-md-6">
+	<iframe width="507.3814697265625" height="313.1341748046875" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQT4g_NqJN5LmvV7XMotgMSA2YK4-qb5kKl6fAyFCd_bCZ9yMkqAozIXxfc4tFQ58Crn6qYSKPmV1TT/pubchart?oid=301337610&amp;format=interactive"></iframe>
+</div>

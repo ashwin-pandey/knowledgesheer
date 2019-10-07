0e809f5188786a79e19c5a2b17aa78a3c662a96b
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-10-07 17:20:23 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -5,11 +5,6 @@ $db['db_user'] = "root";
 $db['db_pass'] = "";
 $db['db_name'] = "knowledgesheer";
 
-// $db['db_host'] = "localhost";
-// $db['db_user'] = "knowpkqm_knowledgesheer";
-// $db['db_pass'] = "Ashwin@123";
-// $db['db_name'] = "knowpkqm_knowledgesheer";
-
 foreach($db as $key => $value){
 	define(strtoupper($key), $value);
 }

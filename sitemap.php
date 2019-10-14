5d8578e17e9d36fbc602586c12b343c27d4c94b9
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-10-14 17:59:36 +0530

    static sitemap

--- sitemap.php
+++ sitemap.php
@@ -6,36 +6,27 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 include './includes/db.php';
 include './admin/functions.php';
 
-echo '<urlset xmlns="https://sitemaps.org/schemas/sitemap/0.9">';
+?>
 
-// Static links
-echo '
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
 <!-- static links -->
 <url>
     <loc>http://knowledgesheer.com</loc></url>
-    <changefreq>daily</changefreq>
 <url>
     <loc>http://knowledgesheer.com/index</loc>
-    <changefreq>daily</changefreq>
 </url>
 <url>
     <loc>http://knowledgesheer.com/quotes</loc>
-    <changefreq>daily</changefreq>
 </url>
 <url>
     <loc>http://knowledgesheer.com/all_categories</loc>
-    <changefreq>daily</changefreq>
 </url>
 <url>
     <loc>http://knowledgesheer.com/login</loc>
-    <changefreq>weekly</changefreq>
 </url>
 <url>
     <loc>http://knowledgesheer.com/registration</loc>
-    <changefreq>weekly</changefreq>
 </url>
-';
 
-echo '</urlset>';
-
-?>
+</urlset>
